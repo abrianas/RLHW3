0a105c8a5035cf8884487ae4648fb81ed07acdc9
Meghna Gummadi <meghnag@seas.upenn.edu>
2019-11-14 16:05:31 -0500

    Pendulum first commit

--- pendulum.py
+++ pendulum.py
@@ -1,9 +1,15 @@
+"""
+Code taken from OpenAIGym
+https://github.com/openai/gym/blob/master/gym/envs/classic_control/pendulum.py
+
+http://incompleteideas.net/sutton/MountainCar/MountainCar1.cp
+permalink: https://perma.cc/6Z2N-PFWC
+"""
+
 import numpy as np
 from os import path
 
 class Continuous_Pendulum:
-    
-
     def __init__(self, g=10.0):
         self.max_speed=8
         self.max_torque=2.
@@ -14,7 +20,10 @@ class Continuous_Pendulum:
         high = np.array([1., 1., self.max_speed])
         self.action_space = np.array([self.max_torque, -self.max_torque])
         self.observation_space = np.array([-high, high])
+	
 
+        self.iter = 0
+        self.maxiter = 200
         self.seed()
 
     def seed(self, seed=None):
@@ -37,11 +46,13 @@ class Continuous_Pendulum:
         newthdot = np.clip(newthdot, -self.max_speed, self.max_speed) #pylint: disable=E1111
 
         self.state = np.array([newth, newthdot])
-        return self._get_obs(), -costs, False, {}
+        self.iter += 1
+        done = (self.iter == self.maxiter)
+        return self._get_obs(), -costs, done, {}
 
     def reset(self):
+        self.iter = 0
         high = np.array([np.pi, 1])
-        #self.state = np.array([np.random.uniform(low=-high, high=-high)])
         self.state = np.random.uniform(low=-high, high=-high)
         self.last_u = None
         return self._get_obs()
@@ -53,3 +64,23 @@ class Continuous_Pendulum:
 
 def angle_normalize(x):
     return (((x+np.pi) % (2*np.pi)) - np.pi)
+
+
+if __name__ == "__main__":
+    print("hi")
+    env = Continuous_Pendulum()
+    env.reset()
+    done = False
+    action = np.array([0])
+    rewards = []
+    while not done:
+        state, reward, done, _ = env.step(action)
+        rewards.append(reward)
+
+    print(len(rewards))
+
+
+
+
+
+
--- pendulum_reinforce_gaussianbaseline.py
+++ pendulum_reinforce_gaussianbaseline.py
@@ -1,57 +1,70 @@
 import numpy as np
 import scipy.signal
-import gym
+
 import pdb
 import matplotlib.pyplot as plt
 from pendulum import *
-import pdb 
+import pdb
 
 class ContinuousPolicy(object):
-    def __init__(self, num_states, num_actions):
-        self.num_states = num_states 
+    def __init__(self, num_states, num_actions, no_rbf):
+        self.num_states = num_states
         self.num_actions = num_actions
-        # here are the weights for the policy - you may change this initialization       
-        self.weights = np.zeros((self.num_states, self.num_actions))
+        # here are the weights for the policy - you may change this initialization
+        self.weights = np.random.random((no_rbf, self.num_actions))
 
 
-    # TODO: fill this function in    
-    # it should take in an environment state
-    def act(self, state):
-        pass
-
-    # TODO: fill this function in    
-    # computes the gradient of the discounted return    
-    # at a specific state and action    
-    # use the computed advantage function appropriately.
-    # return the gradient, a (self.num_states, self.num_actions) numpy array
-    def compute_gradient(self, state, action, discounted_return,advantage):
-        pass
 
     # TODO: fill this function in
-    # takes a step of gradient ascent given a gradient (from compute_gradient())    
+    # it should take in an environment state
+    def act(self, state):
+
+        sigma = 1.0
+        action = np.random.normal(np.dot(self.weights.T,state),sigma)
+
+        return action
+
+    # TODO: fill this function in
+    # computes the gradient of the discounted return
+    # at a specific state and action
+    # use the computed advantage function appropriately.
+    # return the gradient, a (self.num_states, self.num_actions) numpy array
+    def compute_gradient(self, state, action, advantage):
+
+    sigma = 1.0
+    grad = ((action - np.dot(self.weights.T,state))/sigma**2)*state
+    grad = advantage*grad
+
+    return grad
+    # TODO: fill this function in
+    # takes a step of gradient ascent given a gradient (from compute_gradient())
     # and a step size. adjust self.weights
     def gradient_step(self, grad, step_size):
-        pass
+        self.weights = self.weights + grad*step_size
 
-#design gaussian baseline
-class GaussianValueEstimator(object):
-    def __init__(self, num_states,num_actions):
-        self.num_states = num_states
-        self.num_actions = num_actions 
-        
-        #initial value estimates or weights of the value estimator are set to zero. 
-        self.values = np.zeros((self.num_states))
+
+#design linear baseline
+class LinearValueEstimator(object):
+    def __init__(self, num_rbf):
+        self.num_rbf = num_rbf
+
+	self.weights = np.zeros(num_rbf)
+
 
     # TODO: fill this function in
     #takes in a state and predicts a value for the state
     def predict(self,state):
-        pass
+        ## here state is the rbf feacture vector
+        value_predicted  = np.matmul(state,self.weights)
+        return value_predicted
 
     # TODO: fill this function in
-    # construct a suitable loss function and use it to update the 
+    # construct a suitable loss function and use it to update the
     # values of the value estimator. choose suitable step size for updating the value estimator
-    def update(self,state,value_estimate,target,value_step_size):
-        pass
+    def update(self, state, value_estimate, target, value_step_size):
+        delta = target - value_estimate
+        gradientV = state
+        self.weights = self.weights + value_step_size*delta*gradientV
 
 
 # TODO: fill this function in
@@ -59,44 +72,143 @@ class GaussianValueEstimator(object):
 # and returns a list of discounted rewards
 # Ex. get_discounted_returns([1, 1, 1], 0.5)
 # should return [1.75, 1.5, 1]
-
 def get_discounted_returns(rewards, gamma):
-    pass
 
-# TODO: fill this function in 
+    discount_rewards = np.zeros(len(rewards))
+    temp = 0
+    for t in reversed(range(0,len(rewards))):
+        temp = temp*gamma+rewards[t]
+        discount_rewards[t] = temp
+    return discount_rewards
+
+# TODO: fill this function in
 # this will take in an environment, GridWorld
-# a continuous policy 
+# a continuous policy
 # a value estimator,
 # a discount rate, gamma
 # and the number of episodes you want to run the algorithm for
-# make sure to add in the baseline computation here. 
-# Using the computed baseline, compute the advantage. 
+# make sure to add in the baseline computation here.
+# Using the computed baseline, compute the advantage.
 # Use this advantage in the policy gradient calculation
-def reinforce(env, policy,value_estimator):
-        pass
+def reinforce(env, policy, value_estimator, gamma, num_episodes,value_step_size, learning_rate,base_flag):
 
-    
+    episode_rewards = []
 
+
+    for e in range(num_episodes):
+
+        state = env.reset()
+        state = compute2dstate(state)
+        feature_state = rbf(state, centers, rbf_sigma)
+
+        episode_log = []
+        iter = 0
+        done = False
+
+        while done == False and iter < 1000:
+            # get action
+
+            action = policy.act(feature_state)
+            # get next step, reward and chek whether reached goal
+            next_state, reward, done, blah= env.step(action)
+            next_state = compute2dstate(next_state)
+
+            # store the state, action, reward and next state
+            episode_log.append([feature_state, action, reward, next_state])
+
+            feature_state = rbf(next_state, centers, rbf_sigma)
+            iter += 1
+
+
+        episode_log = np.asarray(episode_log)
+        rewards = episode_log[:,2]
+        episode_rewards.append(np.sum(rewards))
+
+        if done:
+            discount_rewards = get_discounted_returns(rewards, gamma)
+            target = np.asarray(discount_rewards)
+            for t in range(0,len(episode_log)):
+                if base_flag: # reinforce with baseline
+                    value_estimate = value_estimator.predict(episode_log[t,0])
+                else: # reinforce w/o baseline
+                    value_estimate = 0
+                advantage = target[t] - value_estimate
+                grads = policy.compute_gradient(episode_log[t,0], episode_log[t,1], discount_rewards[t],
+                advantage)
+
+                # update weight parameters
+                value_estimator.update(episode_log[t,0], value_estimate, target[t], value_step_size)
+                policy.gradient_step(grads, learning_rate)
+
+    return episode_rewards
+
+def compute2dstate(state):
+    theta = np.arctan(state[1]/state[0])
+    state_2d = np.array([theta, state[2]])
+    return state_2d
+
+
+def rbf(state, centers, rbf_sigma):
+    ## input state, return rbf features pi
+    phi = []
+    for c in range(0, len(centers)):
+        rbf_eval =  np.exp(-np.linalg.norm(state - centers[c,:])**2/(2*(rbf_sigma**2)))
+        phi.append(rbf_eval)
+
+    return phi
+
+def computeRBFcenters(th_low, th_high, th_dot_low, th_dot_high, no_rbf):
+    theta = np.linspace(th_low, th_high, no_rbf)
+    thetadot = np.linspace(th_dot_low, th_dot_high, no_rbf)
+    theta_c, thetadot_c = np.meshgrid(theta, thetadot)
+    centers = []
+
+    for i in range(0,no_rbf):
+        for j in range(0,no_rbf):
+            c = [theta_c[i,j], thetadot_c[i,j]]
+            centers.append(c)
+
+    print(centers)
+    return centers
 
 
 if __name__ == "__main__":
-    
     env = Continuous_Pendulum()
-    
-    pdb.set_trace()
-    
-    policy = ContinuousPolicy(num_states, num_actions)
-    value_estimator = GaussianValueEstimator(num_states, num_actions)
-    reinforce(env, policy,value_estimator)
+    num_episodes = 5000
+    gamma = 0.9
+    learning_rate = 0.0001
+    value_step_size = 0.00000005
 
-    #Test time
+    th_low = -np.pi
+    th_high = np.pi
+    th_dot_low = -8.0
+    th_dot_high = 8.0
+    no_rbf = 4
+    rbf_sigma = 1.0/(no_rbf + 1)
+    centers = computeRBFcenters(th_low, th_high, th_dot_low, th_dot_high, no_rbf)
+    no_centers = len(centers)
+
+
+
+    # TODO: define num_states and num_actions
+    policy = ContinuousPolicy(num_states, num_actions)
+    value_estimator = LinearValueEstimator(no_centers)
+    reinforce(env, policy, value_estimator, gamma, num_episodes,value_step_size, learning_rate,True)
+
+    # Test time
     state = env.reset()
-    env.print()
+    # env.print()
     done = False
+    state_hist = []
     while not done:
         input("press enter:")
         action = policy.act(state)
         state, reward, done = env.step(action)
-        # env.print()
+        state_hist.append(state)
 
-
+    # Plotting test time results
+    state_hist = np.array(state_hist)
+    plt.plot(state_hist[0, :])
+    plt.xlabel("time (s)")
+    plt.ylabel("angle (rad)")
+    plt.show()
